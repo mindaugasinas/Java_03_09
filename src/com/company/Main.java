bef2e0830b22404171b51dac4d2fa95c15eac7fc
mindaugasinas <sinkeviciusm@gmail.com>
2018-02-01 09:14:07 +0200

    Skaiciuokle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,22 +3,24 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    public static final int ACTION_SUM = 1;
-    public static final int SUBTRACTION = 1;
-    public static final int ACTION_MULTIPLICATION = 1;
-    public static final int ACTION_REPEAT = 1;
+    public static final int ACTION_SUDETIS = 1;
+    public static final int ACTION_ATIMTIS = 2;
+    public static final int ACTION_DAUGYBA = 3;
+    public static final int ACTION_PAKARTOT = 5;
 
     public static void main(String[] args) {
 
 
         Main objektas = new Main();
+        objektas.skaiciuok();
 
-        // write your code here
+
     }
-    public void skaiciuok(){
+
+    public void skaiciuok() {
         boolean arTesti = true;
         Scanner sc = new Scanner(System.in);
-        while(arTesti == true){
+        while (arTesti == true) {
             System.out.println("Iveskite du skaicius");
             float a = sc.nextFloat();
             float b = sc.nextFloat();
@@ -28,23 +30,26 @@ public class Main {
             System.out.println("3 - Sandauga");
             int veiksmas = sc.nextInt();
 
-            switch (veiksmas){
-                case ACTION_SUM:
-                    System.out.println(a+b);
+            switch (veiksmas) {
+                case ACTION_SUDETIS:
+                    System.out.println(a + b);
                     break;
-                case ACTION_MULTIPLICATION
-                    System.out.println(a-b);
+
+                case ACTION_ATIMTIS:
+                    System.out.println(a - b);
                     break;
-                case ACTION_MULTIPLICATION
-                    System.out.println(a+b);
+
+                case ACTION_DAUGYBA:
+                    System.out.println(a * b);
                     break;
+
             }
             System.out.println("Iveskite ar norite kartoti");
             System.out.println("5 - Kartoti");
             int kartojimoSkaicius = sc.nextInt();
 
-            if (kartojimoSkaicius != ACTION_REPEAT){
-                arTesti=false;
+            if (kartojimoSkaicius != ACTION_PAKARTOT) {
+                arTesti = false;
             }
 
 
